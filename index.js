44818451527a16f09f0562016ee6b71b6056113c
osamudiamen <osas.azamegbe@gmail.com>
2020-07-09 16:08:21 +0100

    modified html, added css for budget and expense feedback. added check for empty or negative budget input.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 class UI {
     constructor(){
+        this.budgetFeedback = document.querySelector(".budget-feedback");
+        this.expenseFeedback = document.querySelector(".expense-feedback");
         this.budgetForm = document.getElementById("budget-form");
         this.budgetInput = document.getElementById("budget-input");
         this.expenseForm = document.getElementById("expense-form");
@@ -13,12 +15,50 @@ class UI {
         this.itemId = 0;
 
     }
+
+    //budget form submit
+    submitBudgetForm(){
+        const value = this.budgetInput.value;
+
+        // send slert if input is empty or negative
+        if (value === "" || value < 0){
+            this.budgetFeedback.classList.add("show-item");
+            this.budgetFeedback.innerHTML = `<p>budget can't be empty or negative</p>`;
+
+            // remove alert after 5000 ms
+            const self = this;
+            setTimeout(function(){
+                self.budgetFeedback.classList.remove("show-item");
+            }, 5000)
+        }
+    }
 }
 
 function eventListeners(){
     const budgetForm = document.getElementById("budget-form");
     const expenseForm = document.getElementById("expense-form");
     const expenseList = document.getElementById("expense-list");
+
+    // new instance of UI class
+    const ui = new UI();
+
+    // budget form submit
+    budgetForm.addEventListener('submit', function(event){
+        event.preventDefault();
+        ui.submitBudgetForm();
+    })
+
+    // expense form submit
+    expenseForm.addEventListener('submit', function(e){
+        e.preventDefault();
+    })
+
+    // expense click
+    expenseList.addEventListener('click', function(){
+        
+    })
+
+
 }
 
 document.addEventListener('DOMContentLoaded', function(){
